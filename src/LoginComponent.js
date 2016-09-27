7ab3412546f5bb19585fbca8198db5397dc9832a
Sven <s.schannak@gusti-leder.de>
2016-09-27 14:59:32 +0200

    login to form for pressing enter to submit

--- src/LoginComponent.js
+++ src/LoginComponent.js
@@ -45,6 +45,7 @@ export default class LoginComponent extends Component {
             <div className="panel panel-default">
               <div className="panel-heading"><h3 className="panel-title"><strong>Sign in </strong></h3></div>
               <div className="panel-body">
+              <form>
                 <div className="input-group" style={styleInput}>
                     <span className="input-group-addon"><i className="glyphicon glyphicon-envelope"></i></span>
                     <input type="text" className="form-control" name="username" value={this.state.email} placeholder="email" onChange={this.handleEmailChange.bind(this)} />
@@ -54,6 +55,7 @@ export default class LoginComponent extends Component {
                     <input type="password" className="form-control" name="password" value={this.state.password} placeholder="password" onChange={this.handlePasswordChange.bind(this)} />
                 </div>
                 <button className="btn btn-block" onClick={this.handleClick.bind(this)}>Login</button>
+                </form>
               </div>
             </div>
 
